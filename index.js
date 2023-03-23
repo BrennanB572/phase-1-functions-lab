0c0119f8bd29ba80bccfc89694a703a9edbe85f1
Brennan Bradley <Brennanb572@gmail.com>
2023-03-23 12:20:09 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    return Math.abs(location - 42);
+  }
+
+  function distanceFromHqInFeet(location) {
+    return Math.abs(location - 42) * 264;
+  }
+
+    function distanceTravelledInFeet(start, destination){
+        const feet = 264;
+        let distance;
+        if (start >= destination){
+            distance = (start - destination) * feet;
+        } else {
+            distance = (destination - start) * feet;
+        }
+        return distance;
+    }
+
+    function calculatesFarePrice(start, destination){
+        const distance = distanceTravelledInFeet(start, destination);
+        const freeTrip = 400;
+        let total;
+        if (distance <= freeTrip){
+            return 0; 
+    } else if (distance > 400 && distance < 2000){
+        total = (distance - freeTrip) * 0.02;
+    } else if (distance > 2000 && distance < 2500){
+        total = 25;
+    } else 
+        total = 'cannot travel that far'
+        return total;
+    }
+    
